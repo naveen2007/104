a7533470736309f04f6de90fbd2166353180a96e
naveen2007 <72682781+naveen2007@users.noreply.github.com>
2021-10-15 00:44:24 -0700

    Add files via upload

---
+++ mode.py
@@ -0,0 +1,31 @@
+import csv 
+from collections import Counter
+
+with open('data.csv', newline = '') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+file_data.pop(0)    
+new_data = []
+for i in range(len(file_data)):
+    num = file_data[i][1]
+    new_data.append(float(num))
+data = Counter(new_data)
+mode_data_for_range = {
+                        '50-60':0,
+                        '60-70':0,
+                        '70-80':0
+                    }
+for height,occurance in data.items():
+    if 50 < float(height) < 60:
+        mode_data_for_range['50-60'] += occurance
+    elif 60 < float(height) < 70:
+        mode_data_for_range['60-70'] += occurance
+    elif 70 < float(height) < 80:
+        mode_data_for_range['50-60'] += occurance
+mode_range,mode_occourance = 0,0
+for range,occurance in mode_data_for_range.items():
+    if occurance > mode_occourance:
+        mode_range, mode_occurance = [int(range.split("-")[0]), int(range.split("-")[1])], occurance
+mode = float((mode_range[0]+mode_range[1])/2)
+print(mode)
+
